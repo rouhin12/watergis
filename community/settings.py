ec56cf4d4dd1a9d6df00e41a7943bdc47ef8f624
Prarthana <prarthanasubudhi@gmail.com>
2024-05-27 14:09:06 +0530

    Added more column to query builder

--- community/settings.py
+++ community/settings.py
@@ -18,13 +18,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# GDAL PATH
-GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal306.dll'
-
-# GEOS PATH
-GEOS_LIBRARY_PATH = r'C:\OSGeo4W\bin\geos_c.dll'
-
-PROJ_LIBRARY_PATH = r'C:\OSGeo4W64\share\proj'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
