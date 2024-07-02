bab6085391e2f55031ecca008cb57e5d7045a410
monikashah <monikapatira@gmail.com>
2024-07-02 12:45:07 +0530

    Timeseries added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('get_layer/', views.get_layer, name='get_layer'),
     # path('get_villages_near_river/', views.get_villages_near_river, name='get_villages_near_river'),
     path('intersecting_villages/', views.intersecting_villages, name='intersecting_villages'),
+    path('timeseries',views.timeseries,name='timeseries'),
 
 ]
     
